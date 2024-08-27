d539b7551ddb52df826e188d563454e4a298d085
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-08-27 16:24:22 -0400

    feedback

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,11 +1,11 @@
 // TODO: Import BrowserRouter and wrap the entire app with it
-import 
+// import 
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import RobotProvider from './context/RobotProvider'
 
-
+/** FEEDBACK: You forgot to wrap app with BrowserRouter! */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <RobotProvider>
